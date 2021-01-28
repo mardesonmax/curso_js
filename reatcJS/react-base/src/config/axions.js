25f5bc58b6e55c173b510c4c55d10498f618b355
Mardeson Pereira <maxpb777@gmail.com>
2021-01-27 23:48:13 -0300

    Adicionando arquivos

--- reatcJS/react-base/src/config/axions.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  // URL base da API
-  baseURL: 'https://api-curso-js.herokuapp.com/',
-});
--- reatcJS/react-base/src/strore/modules/rootReduce.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import example from './example/reduce';
-
-export default combineReducers({
-  example,
-});
